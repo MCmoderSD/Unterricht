048d297c6b699ecd7d0320b066167f4c3001fe91
mcmodersd <mcmodersd@web.de>
2022-12-23 21:19:58 +0100

    Linux Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,30 +3,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            try {
-                Runtime.getRuntime().exec("cmd /c start \"\" java -jar Unterricht.jar a");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        String OperatingSystem = System.getProperty("os.name");
+        if (OperatingSystem.contains("Windows")) {
+            if (args.length == 0) {
+                try {
+                    Runtime.getRuntime().exec("cmd /c start \"\" java -jar Unterricht.jar a");
+                    modeSelector();
+                } catch (IOException e) {
+                    modeSelector();
+                    throw new RuntimeException(e);
+                }
             }
         } else {
-            System.out.println("Continue Through Console or GUI?");
-            System.out.println("1. Console");
-            System.out.println("2. GUI");
+            modeSelector();
+        }
+    }
 
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            if (input.equals("1")) {
-                new Console.Loader();
-            } else if (input.equals("2")) {
-                new UI.StartPage();
-            } else if (input.equals("Console") || input.equals("console")) {
-                new Console.Loader();
-            } else if (input.equals("GUI") || input.equals("gui")) {
-                new UI.StartPage();
-            } else {
-                System.out.println("Invalid Input");
-            }
+    private static void modeSelector() {
+        System.out.println("Continue Through Console or GUI?");
+        System.out.println("1. Console");
+        System.out.println("2. GUI");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        if (input.equals("1")) {
+            new Console.Loader();
+        } else if (input.equals("2")) {
+            new UI.StartPage();
+        } else if (input.equals("Console") || input.equals("console")) {
+            new Console.Loader();
+        } else if (input.equals("GUI") || input.equals("gui")) {
+            new UI.StartPage();
+        } else {
+            System.out.println("Invalid Input");
         }
     }
 }
